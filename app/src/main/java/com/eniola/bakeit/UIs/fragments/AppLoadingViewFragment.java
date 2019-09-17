58f0268bbb9bf39b8d1c3f99c47cc69928d26dde
IPOOLA Eniola <eniolaipoola@gmail.com>
2019-09-17 17:50:55 +0100

    Remove unused line of code

--- app/src/main/java/com/eniola/bakeit/UIs/fragments/AppLoadingViewFragment.java
+++ app/src/main/java/com/eniola/bakeit/UIs/fragments/AppLoadingViewFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-o
+
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.DialogFragment;
