4503d73b0f1501c92ac60e5b708575577af8099e
IPOOLA Eniola <eniolaipoola@gmail.com>
2019-09-17 17:50:36 +0100

    Use databinding for error and loading dialog fragment

--- app/src/main/java/com/eniola/bakeit/UIs/fragments/AppLoadingViewFragment.java
+++ app/src/main/java/com/eniola/bakeit/UIs/fragments/AppLoadingViewFragment.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
+o
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.DialogFragment;
 
 import com.eniola.bakeit.R;
+import com.eniola.bakeit.databinding.FragmentLoadingViewBinding;
 
 public class AppLoadingViewFragment extends DialogFragment {
 
@@ -25,12 +26,13 @@ public class AppLoadingViewFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_loading_view, container, false);
-        TextView progressTextView = rootView.findViewById(R.id.progressTextView);
+        FragmentLoadingViewBinding fragmentLoadingViewBinding = DataBindingUtil.inflate(inflater,
+                R.layout.fragment_loading_view, container, false);
+        View rootView = fragmentLoadingViewBinding.getRoot();
 
         if (getArguments() != null) {
             String progressMessage = getArguments().getString("progressMessage");
-            progressTextView.setText(progressMessage);
+            fragmentLoadingViewBinding.progressTextView.setText(progressMessage);
         }
 
         return rootView;
